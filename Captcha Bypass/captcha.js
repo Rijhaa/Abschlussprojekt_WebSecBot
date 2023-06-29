86b358d9739277699b58da9887c739216b15adf0
Rijhaa <113598974+Rijhaa@users.noreply.github.com>
2023-06-29 14:05:22 +0200

    Update captcha.js

--- Captcha Bypass/captcha.js
+++ Captcha Bypass/captcha.js
@@ -1,88 +1,71 @@
 const puppeteer = require('puppeteer');
 const request = require('request-promise-native');
 const poll = require('promise-poller').default;
-
-// Dein API-Schlüssel für den Captcha-Lösungsdienst
 const apiKey = 'adbc7d647ca8f20eebd39808f859aa81';
 
-// Konfiguration für das Captcha und die Zielseite
 const config = {
-  sitekey: '6LdTUqEmAAAAAAe1-sUMJBC7a93qQfYPmlM7v-bs',
-  pageurl: 'https://rijha.ictblj.ch/', // Ersetze es durch die URL deiner Zielseite
-  apiSubmitUrl: 'https://2captcha.com/in.php',
-  apiRetrieveUrl: 'https://2captcha.com/res.php'
+sitekey: '6LezxKEmAAAAAHZ6hV7YrxPJgTiFyQfS4pRML2ja',
+pageurl: 'https://joerg.ictblj.ch',
+apiSubmitUrl: 'https://2captcha.com/in.php',
+apiRetrieveUrl: 'https://2captcha.com/res.php'
 };
-
-// Funktion, um die Anfrage zur Captcha-Lösung zu initiieren
+// Funktion zum Initiieren der Captcha-Lösungsanfrage
 async function initiateCaptchaRequest(apiKey) {
-  const formData = {
-    method: 'userrecaptcha',
-    googlekey: config.sitekey,
-    key: apiKey,
-    pageurl: config.pageurl,
-    json: 1
-  };
-  const response = await request.post(config.apiSubmitUrl, { form: formData });
-  return JSON.parse(response).request;
+const formData = {
+method: 'userrecaptcha',
+googlekey: config.sitekey,
+key: apiKey,
+pageurl: config.pageurl,
+json: 1
+};
+const response = await request.post(config.apiSubmitUrl, { form: formData
+});
+return JSON.parse(response).request;
 }
-
-// Funktion, um den Captcha-Lösungsdienst nach der Antwort abzufragen
-async function pollForRequestResults(apiKey, requestId, retries = 30, interval = 1500, delay = 15000) {
-  await timeout(delay);
-  try {
-    return await poll({
-      taskFn: requestCaptchaResults(apiKey, requestId),
-      interval,
-      retries
-    });
-  } catch (error) {
-    if (Array.isArray(error) && error.includes('ERROR_WRONG_CAPTCHA_ID')) {
-      console.error('Die Captcha-ID ist ungültig oder abgelaufen.');
-      return null;
-    }
-    throw error;
-  }
+// Funktion zum Überprüfen der Captcha-Lösungsergebnisse
+async function pollForRequestResults(apiKey, requestId, retries = 30, interval
+= 1500, delay = 15000) {
+await timeout(delay);
+try {
+return await poll({
+taskFn: requestCaptchaResults(apiKey, requestId),
+interval,
+retries
+});
+} catch (error) {
+if (Array.isArray(error) && error.includes('ERROR_WRONG_CAPTCHA_ID')) {
+console.error('The captcha ID is invalid or expired.');
+return null;
 }
-
-// Funktion, um die Captcha-Lösungsantwort abzurufen
+throw error;
+}
+}
+// Funktion zum Abrufen der Captcha-Lösungsergebnisse
 function requestCaptchaResults(apiKey, requestId) {
-  const url = `${config.apiRetrieveUrl}?key=${apiKey}&action=get&id=${requestId}&json=1`;
-  return async function () {
-    const rawResponse = await request.get(url);
-    const response = JSON.parse(rawResponse);
-    if (response.status === 0) {
-      throw new Error('Captcha-Lösung läuft noch.');
-    }
-    return response.request;
-  };
+const url =
+`${config.apiRetrieveUrl}?key=${apiKey}&action=get&id=${requestId}&json=1`;
+return async function () {
+const rawResponse = await request.get(url);
+const response = JSON.parse(rawResponse);
+if (response.status === 0) {
+throw new Error('Captcha solving in progress');
+Rijha Ehtsham Dokumentation Abschlussprojekt WebSecBot 07.06.2023
+26
 }
-
+return response.request;
+};
+}
 // Timeout-Hilfsfunktion
 function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 (async function main() {
-  try {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    console.log(`Gehe zu ${config.pageurl}`);
-    await page.goto(config.pageurl);
-    // Implementiere die Schritte, um die Benutzername- und Passwortfelder auszufüllen
-    // mit page.type()
-    const requestId = await initiateCaptchaRequest(apiKey);
-    const response = await pollForRequestResults(apiKey, requestId);
-    console.log('Captcha solved:', response);
-    // Implementiere den Schritt, um die gelöste Captcha-Antwort in das
-    // reCAPTCHA-Eingabefeld mit page.evaluate() einzugeben
-    // Abschicken des Anmeldeformulars mit page.click()
-    // Beispiel: await page.evaluate(() => {
-    //   const captchaResponse = document.getElementById('g-recaptcha-response');
-    //   captchaResponse.value = response; // Die gelöste Captcha-Antwort
-    // });
-    // Beispiel: await page.click('#submit-button');
-    await browser.close();
-  } catch (error) {
-    console.error('Es ist ein Fehler aufgetreten:', error);
-  }
+const browser = await puppeteer.launch({ headless: false });
+const page = await browser.newPage();
+console.log(`Navigate to ${config.pageurl}`);
+await page.goto(config.pageurl);
+const requestId = await initiateCaptchaRequest(apiKey);
+const response = await pollForRequestResults(apiKey, requestId);
+console.log('Captcha solved:', response);
+await browser.close();
 })();
